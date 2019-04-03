b53c1dda9b016107cb2225740bedc73c3dd52b0d
Ionut Danila <hello@ionutdanila.com>
2019-04-03 10:12:05 +0300

    Update to latest FFmpeg NuGet Package

--- FFmpegInterop.MediaPlayerSample/MainPage.xaml.cs
+++ FFmpegInterop.MediaPlayerSample/MainPage.xaml.cs
@@ -1,19 +1,6 @@
 ﻿using FFmpegInterop.MediaPlayerSample.Helpers;
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
-using Windows.Media.Core;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
 
 namespace FFmpegInterop.MediaPlayerSample
 {
