04acfa3058faeaff466e136ff4cebdf2c06841ee
Ionut Danila <hello@ionutdanila.com>
2019-05-06 08:58:45 +0300

    Update NuGet Package and sample application to the latest FFmpeg.UWP Package (4.1.3)

--- FFmpegInterop.MediaPlayerSample/MainPage.xaml.cs
+++ FFmpegInterop.MediaPlayerSample/MainPage.xaml.cs
@@ -7,7 +7,8 @@ namespace FFmpegInterop.MediaPlayerSample
     public sealed partial class MainPage : Page
     {
         //private string streamUri = "rtsp://184.72.239.149/vod/mp4:BigBuckBunny_175k.mov";
-        private string streamUri = "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4";
+        //private string streamUri = "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4";
+        private string streamUri = "http://mirrors.standaloneinstaller.com/video-sample/jellyfish-25-mbps-hd-hevc.m4v";
 
         public MainPage()
         {
